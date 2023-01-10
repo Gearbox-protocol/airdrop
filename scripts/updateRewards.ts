343b997ae6af244beacc596d23915188f52d8838
Mikael <mikael.lazarev@gmail.com>
2023-01-10 10:08:13 +0100

    fix: imports fixed

--- scripts/updateRewards.ts
+++ scripts/updateRewards.ts
@@ -4,20 +4,20 @@ import { BigNumber } from "ethers";
 import * as fs from "fs";
 import { ethers } from "hardhat";
 
-import { computeCampaigns } from "../core/compute/campaign";
-import { computeCreditManagers } from "../core/compute/creditManager";
-import { computePools } from "../core/compute/pool";
 import { CSVExport } from "../core/csv/csvExport";
 import { parseBalanceMap } from "../core/merkle/parse-accounts";
 import { formatGear } from "../core/utils/formatter";
-import { loadPrevMerkle } from "../core/utils/prevMerkle";
 import { saveMerkle } from "../core/utils/saveMerkle";
 import {
   IAddressProvider__factory,
   IAirdropDistributor__factory,
   IERC20__factory,
 } from "../types";
+import { computeCampaigns } from "./compute/campaign";
+import { computeCreditManagers } from "./compute/creditManager";
+import { computePools } from "./compute/pool";
 import { mapToClaimed } from "./lib";
+import { loadPrevMerkle } from "./prevMerkle";
 
 export async function updatePoolRewards() {
   const distributed: Record<string, BigNumber> = {};
